619d1d72a777c16bbdba647d06aa06e7da64af27
Philip William Ventura <philipwilliamventura@gmail.com>
2025-02-02 20:48:45 -0500

    mlr matrices and hyperparameters

--- Multivariate Linear regression part2/mlr.py
+++ Multivariate Linear regression part2/mlr.py
@@ -3,3 +3,18 @@ import pandas as pd
 import matplotlib.pyplot as plt
 
 my_data = pd.read_csv("home.txt", names=["size", "bedroom", "price"])
+
+#NORMALIZATION
+my_data = (my_data - my_data.mean()) / my_data.std()
+
+#CREATE MATIXES
+X = my_data.iloc[:,0:2]
+ones = np.ones([X.shape[0],1])
+X = np.concatenate((ones,X), axis=1)
+
+y = my_data.iloc[:,2:3].values
+theta = np.zeros([1,3])
+
+#HYPER PARAMETERS
+alpha = 0.01
+iters = 1000
